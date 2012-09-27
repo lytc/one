9e8930e5bfa6dbe16f779b56aac94f60590c9f3c
LyTran <lcong@digication.com>
2012-09-27 13:27:05 +0700

    move js to src

--- docs/source/javascripts/supports.js
+++
@@ -1,5 +0,0 @@
-(function($) {
-  $.supports = {
-    dataset: !!document.documentElement.dataset
-  }
-})(one)
--- spec/javascripts/arraySpec.js
+++
@@ -1,9 +0,0 @@
-describe('$.arr', function() {
-  it('pad', function() {
-    expect($.arr([]).pad(3, 1).toArray()).toEqual([1, 1, 1])
-    expect($.arr([2, 3]).pad(4, 1).toArray()).toEqual([2, 3, 1, 1])
-  })
-  it('padLeft', function() {
-    expect($.arr([2, 3]).padLeft(4, 1).toArray()).toEqual([1, 1, 2, 3])
-  })
-})
